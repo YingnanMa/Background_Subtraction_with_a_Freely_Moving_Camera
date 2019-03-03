ab8653430b63ec29edde520985ed58681df4f4d8
guanfangdong <dong.guanfang.2014@gmail.com>
2019-03-03 14:25:33 -0700

    add main method

--- OF_two_images.py
+++ OF_two_images.py
@@ -30,27 +30,29 @@ def draw_flow(img, gray, flow, step=16):
 
 
 
-
-car1 = cv2.imread("car1.jpg")
-car2 = cv2.imread("car2.jpg")
-
-prvs = cv2.cvtColor(car1,cv2.COLOR_BGR2GRAY)
-next = cv2.cvtColor(car2,cv2.COLOR_BGR2GRAY)
-
-flow = cv2.calcOpticalFlowFarneback(prvs,next, None, 0.5, 3, 15, 3, 5, 1.2, 0)
-
-#mag, ang = cv2.cartToPolar(flow[...,0], flow[...,1])
-
-#hsv = np.zeros_like(car1)
-#hsv[...,1] = 255
-#hsv[...,0] = ang*180/np.pi/2
-#hsv[...,2] = cv2.normalize(mag,None,0,255,cv2.NORM_MINMAX)
-#rgb = cv2.cvtColor(hsv,cv2.COLOR_HSV2BGR)
-
-#cv2.imshow('car2',rgb)
-
-cv2.imshow('detect preview', draw_flow(car2, prvs, flow, 16))
-
-
-k = cv2.waitKey(0)
-print(flow)
+def main():
+    car1 = cv2.imread("car1.jpg")
+    car2 = cv2.imread("car2.jpg")
+    
+    prvs = cv2.cvtColor(car1,cv2.COLOR_BGR2GRAY)
+    next = cv2.cvtColor(car2,cv2.COLOR_BGR2GRAY)
+    
+    flow = cv2.calcOpticalFlowFarneback(prvs,next, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+    
+    #mag, ang = cv2.cartToPolar(flow[...,0], flow[...,1])
+    
+    #hsv = np.zeros_like(car1)
+    #hsv[...,1] = 255
+    #hsv[...,0] = ang*180/np.pi/2
+    #hsv[...,2] = cv2.normalize(mag,None,0,255,cv2.NORM_MINMAX)
+    #rgb = cv2.cvtColor(hsv,cv2.COLOR_HSV2BGR)
+    
+    #cv2.imshow('car2',rgb)
+    
+    cv2.imshow('detect preview', draw_flow(car2, prvs, flow, 16))
+    
+    
+    k = cv2.waitKey(0)
+    print(flow)
+    
+main()
