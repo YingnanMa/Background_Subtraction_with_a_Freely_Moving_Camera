d35a5b09565b99d0675d091a0977638ff72a9105
guanfangdong <dong.guanfang.2014@gmail.com>
2019-03-20 17:19:07 -0600

    make some videos

--- Average_angle_method/BS_average_angle.py
+++ Average_angle_method/BS_average_angle.py
@@ -17,6 +17,50 @@ Returns:
     vis - the image matrix
 """
 
+def easy_thresholding(img, given_matrix, thresholding):
+
+    matrix_shape = given_matrix.shape
+    
+    binary_mask = np.zeros(matrix_shape)
+    foreground_matrix = np.zeros(matrix_shape)
+
+    for x, y in product(range(matrix_shape[0]), range(matrix_shape[1])):
+        if given_matrix[x][y] > thresholding:
+            binary_mask[x][y] = 255
+            foreground_matrix[x][y] = img[x][y]
+
+    return foreground_matrix, binary_mask
+
+
+def get_mask(pca_foreground_matrix, prvs):
+    
+    mean_value = pca_foreground_matrix.mean()
+    
+    mask_matrix = pca_foreground_matrix - mean_value
+    
+    mask_matrix = np.absolute(mask_matrix*3)
+    
+    mask_matrix = mask_matrix.astype(np.uint8)
+    
+    total_number = 0
+    
+    matrix_shape = mask_matrix.shape
+    
+    count = 0
+    
+    for x, y in product(range(matrix_shape[0]), range(matrix_shape[1])):
+        if mask_matrix[x][y] < 100:
+            total_number += mask_matrix[x][y]
+            count += 1
+    
+    average_number = total_number / count + 30
+    
+    foreground_matrix, binary_mask = easy_thresholding(prvs, mask_matrix, average_number)
+    
+    return foreground_matrix, binary_mask
+
+
+
 def draw_flow(img, gray, flow, step=16):
 
     h, w = img.shape[:2]
@@ -88,8 +132,14 @@ def implement_average_magnitude_betweem_two_frames(image1, image2):
     #convert to uint8
     foreground = foreground.astype(np.uint8)
 
+
+    foreground_matrix, binary_mask = get_mask(foreground, prvs)
+    foreground_matrix = foreground_matrix.astype(np.uint8)
+    binary_mask = binary_mask.astype(np.uint8)
+
     #write image
-    cv2.imwrite('foreground_'+str(image1)+'.png',foreground)
+    cv2.imwrite('foreground_'+str(image1)+'.png',foreground_matrix)
+    cv2.imwrite('binary_mask_'+str(image1)+'.png',binary_mask)
 
     #destroy table
     cv2.destroyAllWindows()
@@ -99,7 +149,7 @@ def main():
 
     #implement background subtraction to all frames
     pre = "bear02_0"
-    for i in range(100, 450):
+    for i in range(450, 458):
         implement_average_magnitude_betweem_two_frames(pre + str(i) + ".jpg", pre + str(i+1) + ".jpg")
 
 main()
