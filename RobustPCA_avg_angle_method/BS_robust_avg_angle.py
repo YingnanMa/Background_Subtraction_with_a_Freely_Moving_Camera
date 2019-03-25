dd3a49fe30439431f9d8cecd2f66a578908ab478
YingnanMa <ma4@ualberta.ca>
2019-03-24 23:05:44 -0600

    Update Robust PCA with avg angle method
    Update Robust PCA combined with implementation of avg angle method

--- RobustPCA_avg_angle_method/BS_robust_avg_angle.py
+++ RobustPCA_avg_angle_method/BS_robust_avg_angle.py
@@ -156,7 +156,7 @@ def main():
 
     #implement background subtraction to all frames
     pre = "bear02_0"
-    for i in range(455, 458):
+    for i in range(100, 376):
         implement_pca_betweem_two_frames(pre + str(i) + ".jpg", pre + str(i+1) + ".jpg")
 
 main()
